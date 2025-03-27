45313486923a9b335800813c6f69ee87001ec9f9
Durairaja-ss <ceregno23503815@gmail.com>
2025-03-27 06:59:48 +0530

    Add files via upload

--- add.php
+++ add.php
@@ -1,7 +1,9 @@
 <?php
      $connection = mysqli_connect("localhost","root","");
      $db = mysqli_select_db($connection,"dbstu");
+    
 
+ 
 
     if(isset($_POST['submit']))
         {   
@@ -9,8 +11,18 @@
              $rollno = $_POST['rollno'];
              $age = $_POST['age'];
              $mobile = $_POST['mobile'];
+             $gender=$_POST['gender'];
+             $dept=$_POST['department'];
+             
+             $Skills = isset($_POST['Skill']) ? $_POST['Skill'] : [];
+             
+             $SkillsString = !empty($Skills) ? implode(", ", $Skills) : '';
+             
+    
+             
+             
 
-             $sql = "insert into studentapp(name,rollno,age,mobile)values('$name','$rollno','$age','$mobile')";
+             $sql = "insert into studentapp(name,rollno,age,mobile,gender,Skill,department)values('$name','$rollno','$age','$mobile','$gender','$SkillsString','$dept')";
 
              if(mysqli_query($connection,$sql))
              {
@@ -51,15 +63,45 @@
                             <label>Name</label>
                             <input type="text" name="name" class="form-control" placeholder="Enter Name" required>
                         </div> 
+                        
                         <div class="rollno">Rollno</label>
                             <input type="number" name="rollno" class="form-control" placeholder="Enter Rollno" required>
                         </div> 
+                        
                         <div class="age">Age</label>
                             <input type="number" name="age" class="form-control" placeholder="Enter Age" required>
                         </div> 
                         <div class="mobile">Mobile</label>
                             <input type="number" name="mobile" class="form-control" placeholder="Enter Number" required>
                         </div> 
+                        <div class="rdo">       
+                            <label>Choose your gender:</label><br>                        
+                            <input type="radio" id="male" name="gender" value="Male">
+                            <label for="male">Male</label>
+                            <input type="radio" id="female" name="gender" value="Female">
+                            <label for="female">Female</label>
+                        </div>
+                         <div class="chbox">
+                             <label>Select your Skills:</label><br>
+                             <input type="checkbox" name="Skill[]" value="PHP"> PHP <br>
+                             <input type="checkbox" name="Skill[]" value="HTML"> HTML <br>
+                             <input type="checkbox" name="Skill[]" value="C#"> C# <br>
+                             <input type="checkbox" name="Skill[]" value="PYTHON"> PYTHON <br>
+
+                             <br>                       
+                        </div>
+                        <div class="form-group">
+                                    <label for="dept">Department</label><br/>
+                                    <input list="Dept-options" id="dept" name="department">
+                                    <datalist id="Dept-options">
+                                        <option value="CSE">
+                                        <option value="IT">
+                                        <option value="EEE">
+                                        <option value="ECE">
+                                        <option value="AUTOMOBILE">
+                                        <option value="MECHANICAL">
+                                    </datalist>                        
+                                </div>
                         <br>
                         <br>
                         <input type="submit" class="btn btn-primary" name="submit" value="Save">
